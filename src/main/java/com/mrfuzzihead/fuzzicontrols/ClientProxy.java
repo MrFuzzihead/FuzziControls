4a1b6855a497ac3acb7d0d95ef9744e67d43c49e
Duncan Campbell <campbellduncan12@gmail.com>
2026-02-28 16:26:46 -0600

    Initial code push
    Just merging in ExampleMod stuff

---
+++ src/main/java/com/mrfuzzihead/fuzzicontrols/ClientProxy.java
@@ -0,0 +1,8 @@
+package com.mrfuzzihead.fuzzicontrols;
+
+public class ClientProxy extends CommonProxy {
+
+    // Override CommonProxy methods here, if you want a different behavior on the client (e.g. registering renders).
+    // Don't forget to call the super methods as well.
+
+}
